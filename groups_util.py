7970d5c2a25c592874077ad48f373daed019912c
gabriel <gmaher2@hotmail.com>
2016-05-30 15:48:16 -0700

    group reading script update

--- groups_util.py
+++ groups_util.py
@@ -29,7 +29,31 @@ def get_group_from_file(fn):
 				#read next line
 				l = f.readline()
 
+import os
+#get_group_from_file('/home/gabriel/projects/tcl_code/models/OSMSC0001/0001_0001/groups/arch_edge')
+#get_group_from_file('/home/gabriel/projects/tcl_code/models/OSMSC0001/0001_0001/groups/arch_image')
+#get_group_from_file('/home/gabriel/projects/tcl_code/models/OSMSC0001/0001_0001/groups/arch')
+cwd = os.getcwd()
+groups = {}
+groups_edges = {}
+groups_images = {}
 
-get_group_from_file('/home/gabriel/projects/tcl_code/models/OSMSC0001/0001_0001/groups/arch_edge')
-get_group_from_file('/home/gabriel/projects/tcl_code/models/OSMSC0001/0001_0001/groups/arch_image')
-get_group_from_file('/home/gabriel/projects/tcl_code/models/OSMSC0001/0001_0001/groups/arch')
+for fn in os.listdir(cwd+'/models/weiguang/groups'):
+	if "." in fn:
+		continue
+	print fn
+
+	if "_edge" in fn:
+		groupname = fn.replace("_edge", "")
+		groups_edges[groupname] = get_group_from_file(cwd+"/models/weiguang/groups/"+fn)
+
+	elif "_image" in fn:
+		groupname = fn.replace("_image", "")
+		groups_images[groupname] = get_group_from_file(cwd+"/models/weiguang/groups/"+fn)
+
+	else:
+		groups[fn] = get_group_from_file(cwd+"/models/weiguang/groups/"+fn)
+
+print groups_edges.keys()
+print groups_images.keys()
+print groups.keys()
