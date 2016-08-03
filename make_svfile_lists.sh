eae7ebf199188769416f1a0e6a800436e667e2df
gabriel <gmaher2@hotmail.com>
2016-08-03 13:07:47 -0700

    more updates to make_svfile_lists.sh"

--- make_svfile_lists.sh
+++ make_svfile_lists.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 #this file takes in a directory and searches for a set of predefined file patterns
+touch images.txt groups.txt paths.txt edge48.txt edge96.txt
+rm images.txt groups.txt paths.txt edge48.txt edge96.txt
+touch images.txt groups.txt paths.txt edge48.txt edge96.txt
 
 for dir in $1*; do
 	num_images="$(find $dir -name "*OSMSC*-cm.mha" | wc -l)"
@@ -9,16 +12,21 @@ for dir in $1*; do
 
 	num_paths="$(find $dir -name "*.paths" | wc -l)"
 
-	num_edge_48="$(find $dir -name "*-cm_E48.mha" | wc -l)"
+	#num_edge_48="$(find $dir -name "*-cm_E48.mha" | wc -l)"
+	num_edge_48=1
 
 	num_edge_96="$(find $dir -name "*-cm_E96.mha" | wc -l)"
-
-	echo "$num_images, $num_groups, $num_paths, $num_edge_48, $num_edge_96"
 
 	if ([ "$num_images" == "1" ] && [ "$num_groups" == "1" ]  &&
 	 [ "$num_paths" == "1" ] && [ "$num_edge_48" == "1" ] && [ "$num_edge_96" == "1" ]);
 		then
-			echo "$num_images, $num_groups, $num_paths, $num_edge_48, $num_edge_96"
+			#echo "$num_images, $num_groups, $num_paths, $num_edge_48, $num_edge_96"
+			find $dir -name "*OSMSC*-cm.mha" >> images.txt
+			find $dir -name "*groups-cm" -type d >> groups.txt
+			find $dir -name "*.paths" >> paths.txt
+			find $dir -name "*-cm_E48.mha" >> edge48.txt
+			find $dir -name "*-cm_E96.mha" >> edge96.txt
+
 		fi
 
 done
