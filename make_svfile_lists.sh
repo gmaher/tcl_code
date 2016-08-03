ebfd30b42ce7acda955b0c5230b903343d11a8ac
gabriel <gmaher2@hotmail.com>
2016-08-03 12:53:37 -0700

    script to make files for sv part 2

--- make_svfile_lists.sh
+++ make_svfile_lists.sh
@@ -2,30 +2,23 @@
 
 #this file takes in a directory and searches for a set of predefined file patterns
 
-echo "searching "$1
+for dir in $1*; do
+	num_images="$(find $dir -name "*OSMSC*-cm.mha" | wc -l)"
 
-find $1 -name "*-cm.mha" | sort > images.txt
+	num_groups="$(find $dir -name "*groups-cm" -type d | wc -l)"
 
-find $1 -name "*groups*" -type d | sort > groups.txt
+	num_paths="$(find $dir -name "*.paths" | wc -l)"
 
-find $1 -name "*.paths" | sort > paths.txt
+	num_edge_48="$(find $1 -name "*-cm_E48.mha" | wc -l)"
 
-find $1 -name "*-cm_E48.mha" | sort > edge48.txt
+	num_edge_96="$(find $1 -name "*-cm_E96.mha" | wc -l)"
 
-find $1 -name "*-cm_E96.mha" | sort > edge96.txt
+	echo $num_images
 
-#count lines to make sure no duplicates
-echo "Number images"
-find $1 -name "*-cm.mha" | sort | wc -l
+	if ([ "$num_images" == "1" ] && [ "$num_groups" == "1" ]  &&
+	 [ "$num_paths" == "1" ] && [ "$num_edge_48" == "1" ] && [ "$num_edge_96" == "1" ]);
+		then
+			echo "$num_images, $num_groups, $num_paths, $num_edge_48, $num_edge_96"
+		fi
 
-echo "number of groups"
-find $1 -name "*groups*" -type d | wc -l 
-
-echo "number of paths"
-find $1 -name "*.paths" | wc -l
-
-echo "number of 48 edges"
-find $1 -name "*-cm_E48.mha" | wc -l
-
-echo "number of 96 edges"
-find $1 -name "*-cm_E96.mha" | wc -l
+done
