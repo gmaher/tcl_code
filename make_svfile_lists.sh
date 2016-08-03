8ae48f23c7ee3c64d6b7e185d87734282cfc238f
gabriel <gmaher2@hotmail.com>
2016-08-03 12:56:24 -0700

    part 3

--- make_svfile_lists.sh
+++ make_svfile_lists.sh
@@ -13,7 +13,7 @@ for dir in $1*; do
 
 	num_edge_96="$(find $1 -name "*-cm_E96.mha" | wc -l)"
 
-	echo $num_images
+	echo "$num_images, $num_groups, $num_paths, $num_edge_48, $num_edge_96"
 
 	if ([ "$num_images" == "1" ] && [ "$num_groups" == "1" ]  &&
 	 [ "$num_paths" == "1" ] && [ "$num_edge_48" == "1" ] && [ "$num_edge_96" == "1" ]);
