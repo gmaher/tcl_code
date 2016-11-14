c3824bf0a2b3e88477ab82073456f172c20fbb19
marsdenlab <gmaher2@hotmail.com>
2016-11-13 17:17:03 -0800

    added HED and HED_dense to plot code

--- python/evaluate_all.py
+++ python/evaluate_all.py
@@ -14,6 +14,8 @@ from tqdm import tqdm
 import utility.util_data as util_data
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
+import matplotlib.pylab as pylab
+
 from sklearn.metrics import roc_curve
 import configparser
 
@@ -24,6 +26,14 @@ DX = 0.01
 #########################################
 #function definitions
 #########################################
+def get_outputs(seg):
+    contour = utility.listSegToContours(seg, meta_test[1,:,:],
+        meta_test[0,:,:], ISOVALUE)
+    errs = utility.listAreaOverlapError(contour, contours_test)
+    thresh,ts = utility.cum_error_dist(errs,DX)
+    roc = roc_curve(np.ravel(Y_test),np.ravel(seg), pos_label=1)
+    return (contour,errs,thresh,roc)
+
 def contour_plot(x_test, C, extent, labels, colors, filename):
     '''
     function to plot contours on top of images
@@ -38,7 +48,7 @@ def contour_plot(x_test, C, extent, labels, colors, filename):
     '''
     N = len(labels)
     plt.figure()
-    fig_contour, ax_contour = plt.subplots(1,N)
+    fig_contour, ax_contour = plt.subplots(1,N,figsize=(20,8))
     plot_count = 0
     handles = []
     for j in range(0,len(C)):
@@ -46,7 +56,11 @@ def contour_plot(x_test, C, extent, labels, colors, filename):
         label = labels[j]
         color = colors[j]
         ax_contour[j].imshow(x_test[:,:,0], extent=extent,cmap='gray')
-        h, = ax_contour[j].plot(contour[:,0],contour[:,1], color=color,label=label)
+        if contour == []:
+            print "failed contour"
+            h, = ax_contour[j].plot([0],[0], color=color,label=label,linewidth=4)
+        else:
+            h, = ax_contour[j].plot(contour[:,0],contour[:,1], color=color,label=label,linewidth=4)
         handles.append(h)
 
     [a.set_axis_off() for a in ax_contour]
@@ -86,38 +100,43 @@ ROC = {}
 ############################
 # Load Model 1 (FCN)
 ############################
-FCN = load_model('./models/FCN.h5')
-FCN_seg = FCN.predict(X_test)
-FCN_seg = FCN_seg.reshape((Ntest,Pw,Ph))
-FCN_contour = utility.listSegToContours(FCN_seg, meta_test[1,:,:],
-    meta_test[0,:,:], ISOVALUE)
-FCN_errs = utility.listAreaOverlapError(FCN_contour, contours_test)
-FCN_thresh,ts = utility.cum_error_dist(FCN_errs,DX)
-ROC['FCN'] = roc_curve(np.ravel(Y_test),np.ravel(FCN_seg), pos_label=1)
+FCN_seg = np.load('./predictions/FCN.npy').reshape((Ntest,Pw,Ph))
+FCN_contour,FCN_errs,FCN_thresh,FCN_roc =\
+get_outputs(FCN_seg)
+ROC['FCN'] = FCN_roc
 
 ###########################
 # Load Model 2 (OBP_FCN)
 ###########################
-OBP_FCN = load_model('./models/OBP_FCN_output.h5')
-OBP_FCN_seg = OBP_FCN.predict(X_test)[:,:,:,1]
-OBP_FCN_seg = OBP_FCN_seg.reshape((Ntest,Pw,Ph))
-OBP_FCN_contour = utility.listSegToContours(OBP_FCN_seg, meta_test[1,:,:],
-    meta_test[0,:,:], ISOVALUE)
-OBP_FCN_errs = utility.listAreaOverlapError(OBP_FCN_contour, contours_test)
-OBP_FCN_thresh,ts = utility.cum_error_dist(OBP_FCN_errs,DX)
-ROC['OBP_FCN'] = roc_curve(np.ravel(Y_test),np.ravel(OBP_FCN_seg), pos_label=1)
+OBP_FCN_out = np.load('./predictions/OBP_FCN.npy')
+OBP_FCN_seg = OBP_FCN_out[:,:,:,1].reshape((Ntest,Pw,Ph))
+OBP_FCN_contour, OBP_FCN_errs, OBP_FCN_thresh, OBP_roc =\
+get_outputs(OBP_FCN_seg)
+ROC['OBP_FCN'] = OBP_roc
 
 #############################
 # Load Model 3 (OBP_FCN_full)
 #############################
-OBP_full = load_model('./models/OBP_full.h5')
-OBP_full_seg = OBP_full.predict(X_test)
-OBP_full_seg = OBP_full_seg.reshape((Ntest,Pw,Ph))
-OBP_full_contour = utility.listSegToContours(OBP_full_seg, meta_test[1,:,:],
-    meta_test[0,:,:], ISOVALUE)
-OBP_full_errs = utility.listAreaOverlapError(OBP_full_contour, contours_test)
-OBP_full_thresh,ts = utility.cum_error_dist(OBP_full_errs,DX)
-ROC['OBG_FCN'] = roc_curve(np.ravel(Y_test), np.ravel(OBP_full_seg),pos_label=1)
+OBP_full_seg = np.load('./predictions/OBG_FCN.npy').reshape((Ntest,Pw,Ph))
+OBP_full_contour, OBP_full_errs, OBP_full_thresh, OBP_roc =\
+get_outputs(OBP_full_seg)
+ROC['OBG_FCN'] = OBP_roc
+
+#############################
+# Load HED
+#############################
+HED_seg = np.load('./predictions/HED.npy')[0].reshape((Ntest,Pw,Ph))
+HED_contour, HED_errs, HED_thresh, HED_roc =\
+get_outputs(HED_seg)
+ROC['HED'] = HED_roc
+
+#############################
+# Load HED
+#############################
+HED_dense_seg = np.load('./predictions/HED_dense.npy').reshape((Ntest,Pw,Ph))
+HED_dense_contour, HED_dense_errs, HED_dense_thresh, HED_dense_roc =\
+get_outputs(HED_dense_seg)
+ROC['HED_dense'] = HED_dense_roc
 
 #############################
 # Level set
@@ -131,7 +150,7 @@ ls_thresh,ts = utility.cum_error_dist(ls_errs,DX)
 #Figure 1 segmentations
 plt.figure()
 Nplots = 4
-fig_seg, ax_seg = plt.subplots(Nplots,5)
+fig_seg, ax_seg = plt.subplots(Nplots,7,figsize=(35,25))
 for i in range(0,Nplots):
     ax_seg[i,0].imshow(X_test[i,:,:,0],cmap='gray')
     ax_seg[i,0].set_title('Image')
@@ -143,6 +162,10 @@ for i in range(0,Nplots):
     ax_seg[i,3].set_title('OBP_FCN')
     ax_seg[i,4].imshow(OBP_full_seg[i,:,:],cmap='gray')
     ax_seg[i,4].set_title('OBG_FCN')
+    ax_seg[i,5].imshow(HED_seg[i,:,:],cmap='gray')
+    ax_seg[i,5].set_title('HED')
+    ax_seg[i,6].imshow(HED_dense_seg[i,:,:],cmap='gray')
+    ax_seg[i,6].set_title('HED_dense')
     [a.set_axis_off() for a in ax_seg[i, :]]
     [a.set_axis_off() for a in ax_seg[:, i]]
 plt.axis('off')
@@ -151,9 +174,9 @@ plt.savefig('./plots/segs.png')
 
 #Figure 2 contours
 contours_to_plot = [contours_test[0], ls_test[0], FCN_contour[0],
-OBP_FCN_contour[0], OBP_full_contour[0]]
-labels = ['user','level set','FCN','OBP_FCN','OBG_FCN']
-colors = ['green','red','blue','yellow','magenta']
+OBP_FCN_contour[0], OBP_full_contour[0], HED_contour[0], HED_dense_contour[0]]
+labels = ['user','level set','FCN','OBP_FCN','OBG_FCN','HED','HED_dense']
+colors = ['green','red','blue','yellow','magenta','teal','orange']
 contour_plot(X_test[0],contours_to_plot,extents_test[0],labels,colors,'./plots/contours.png')
 
 #Figure 3, IOU
@@ -162,6 +185,8 @@ plt.plot(ts,ls_thresh, color='red', label='level set', linewidth=2)
 plt.plot(ts,FCN_thresh, color='blue', label='FCN', linewidth=2)
 plt.plot(ts,OBP_FCN_thresh, color='yellow', label='OBP_FCN', linewidth=2)
 plt.plot(ts,OBP_full_thresh, color='magenta', label='OBG_FCN', linewidth=2)
+plt.plot(ts,HED_thresh, color='black', label='HED', linewidth=2)
+plt.plot(ts,HED_dense_thresh, color='orange', label='HED_dense', linewidth=2)
 plt.xlabel('IOU error')
 plt.ylabel('Fraction of contours below error')
 plt.legend(loc='lower right')
@@ -172,6 +197,8 @@ plt.figure()
 plt.plot(ROC['FCN'][0],ROC['FCN'][1], color='red', label='FCN', linewidth=2)
 plt.plot(ROC['OBP_FCN'][0],ROC['OBP_FCN'][1], color='blue', label='OBP_FCN', linewidth=2)
 plt.plot(ROC['OBG_FCN'][0],ROC['OBG_FCN'][1], color='green', label='OBG_FCN', linewidth=2)
+plt.plot(ROC['HED'][0],ROC['HED'][1], color='black', label='HED', linewidth=2)
+plt.plot(ROC['HED_dense'][0],ROC['HED_dense'][1], color='orange', label='HED_dense', linewidth=2)
 plt.xlabel('False positive rate')
 plt.ylabel('True positive rate')
 plt.legend(loc='lower right')
---
+++ python/run_all.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+python train.py FCN
+
+python train.py OBP_FCN
+
+python train.py OBG_FCN
+
+python train.py HED
+
+python train.py HED_dense
