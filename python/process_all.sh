02b75601aa7de4bff277d5b7fb0f72f436632fb4
marsdenlab <gmaher2@hotmail.com>
2016-12-20 13:16:27 -0800

    process data shell script

--- python/process_all.sh
+++ python/process_all.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-python process_raw_data.py ./data/192/raw ./data/64/
-
-python process_raw_data.py ./data/192/raw ./data/64/ -t mr
-
-python process_raw_data.py ./data/192/raw ./data/64/ -t ct
-
 python process_raw_data.py ./data/192/raw ./data/192/
 
 python process_raw_data.py ./data/192/raw ./data/192/ -t mr
 
 python process_raw_data.py ./data/192/raw ./data/192/ -t ct
+
+python process_raw_data.py ./data/64/raw ./data/64/
+
+python process_raw_data.py ./data/64/raw ./data/64/ -t mr
+
+python process_raw_data.py ./data/64/raw ./data/64/ -t ct
